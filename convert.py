7b082b53d48adba034b2b42d3b71018518956ab3
Nandan Rao <nandanmarkrao@gmail.com>
2018-10-03 20:14:34 -0700

    fromisoformat and move multiproc to files

--- convert.py
+++ convert.py
@@ -7,7 +7,8 @@ from csv import QUOTE_ALL
 import re, json, logging
 from multiprocessing import Pool, cpu_count
 from argparse import ArgumentParser
-from functools import lru_cache
+from functools import lru_cache, partial
+from itertools import starmap
 
 quote = re.compile(r'"')
 
@@ -26,30 +27,29 @@ class Parser():
         except TypeError:
             delta = timedelta(days = 30)
             truncated = True
-        scraped = self._parse(t)
+        scraped = datetime.fromisoformat(t)
         return datetime.date(scraped - delta), truncated
 
-def parse_date(df, cores):
+def parse_date(df):
     parser = Parser()
-    p = Pool(cores)
-    res = p.starmap(parser.parse_date, zip(df.date, df.scrapeTimestamp))
+    res = starmap(parser.parse_date, zip(df.date, df.scrapeTimestamp))
     dates, truncated = zip(*res)
     return df.assign(date = dates, date_truncated = truncated)
 
-def clean_df(df, cores):
+def clean_df(df):
     return (df
             .assign(
                 category = df.categories.map(lambda l: l[0]),
                 subcategory = df.categories.map(lambda l: l[1]),
                 description = df.description.str.replace(quote, ''))
-            .pipe(lambda df: parse_date(df, cores))
+            .pipe(parse_date)
             .drop(['categories', 'meta', 'reviews'], 1))
 
-def convert(infile, outfile, fs, cores):
+def convert(infile, outfile, fs):
     with fs.open(infile, 'r') as f:
         df = pd.DataFrame([json.loads(l) for l in f.readlines()])
     with fs.open(outfile, 'w') as f:
-        clean_df(df, cores).to_csv(f, index=False, quoting=QUOTE_ALL)
+        clean_df(df).to_csv(f, index=False, quoting=QUOTE_ALL)
 
 def make_outfile(infile, folder, i = 1):
     parts = infile.split('/')
@@ -58,15 +58,17 @@ def make_outfile(infile, folder, i = 1):
 
 parser = ArgumentParser('Convert JL')
 parser.add_argument('folders', nargs='+')
-parser.add_argument('--cores', default = cpu_count())
+parser.add_argument('--cores', default=cpu_count())
 
 if __name__ == '__main__':
     load_dotenv()
     args = parser.parse_args()
     fs = s3fs.S3FileSystem()
+    pool = Pool(int(args.cores))
     for folder in args.folders:
+        logging.info('CONVERTING: {}'.format(folder))
         infiles = fs.ls(folder)
-        outfiles = [make_outfile(f, 'indeed-csvs') for f in infiles]
-        for i,o in zip(infiles, outfiles):
-            logging.info('CONVERTING: {}'.format(i))
-            convert(i,o,fs, args.cores)
+        outfiles = [make_outfile(f, 'indeed-csvs-test') for f in infiles]
+        pool.starmap(partial(convert, fs=fs), zip(infiles, outfiles))
+    pool.join()
+    pool.close()
