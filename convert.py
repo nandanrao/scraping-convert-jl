8aff38aaf229afe9fb45810ce786c3d941b33c54
Nandan Rao <nandanmarkrao@gmail.com>
2018-10-03 19:52:04 -0700

    Added cores argument

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from csv import QUOTE_ALL
 import re, json, logging
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 from argparse import ArgumentParser
 
 quote = re.compile(r'"')
@@ -27,27 +27,27 @@ class Parser():
         scraped = self._parse(t)
         return datetime.date(scraped - delta), truncated
 
-def parse_date(df):
+def parse_date(df, cores):
     parser = Parser()
-    p = Pool()
+    p = Pool(cores)
     res = p.starmap(parser.parse_date, zip(df.date, df.scrapeTimestamp))
     dates, truncated = zip(*res)
     return df.assign(date = dates, date_truncated = truncated)
 
-def clean_df(df):
+def clean_df(df, cores):
     return (df
             .assign(
                 category = df.categories.map(lambda l: l[0]),
                 subcategory = df.categories.map(lambda l: l[1]),
                 description = df.description.str.replace(quote, ''))
-            .pipe(parse_date)
+            .pipe(lambda df: parse_date(df, cores))
             .drop(['categories', 'meta', 'reviews'], 1))
 
-def convert(infile, outfile, fs):
+def convert(infile, outfile, fs, cores):
     with fs.open(infile, 'r') as f:
         df = pd.DataFrame([json.loads(l) for l in f.readlines()])
     with fs.open(outfile, 'w') as f:
-        clean_df(df).to_csv(f, index=False, quoting=QUOTE_ALL)
+        clean_df(df, cores).to_csv(f, index=False, quoting=QUOTE_ALL)
 
 def make_outfile(infile, folder, i = 1):
     parts = infile.split('/')
@@ -56,8 +56,10 @@ def make_outfile(infile, folder, i = 1):
 
 parser = ArgumentParser('Convert JL')
 parser.add_argument('folders', nargs='+')
+parser.add_argument('--cores', default = cpu_count())
 
 if __name__ == '__main__':
+    load_dotenv()
     args = parser.parse_args()
     fs = s3fs.S3FileSystem()
     for folder in args.folders:
@@ -65,4 +67,4 @@ if __name__ == '__main__':
         outfiles = [make_outfile(f, 'indeed-csvs') for f in infiles]
         for i,o in zip(infiles, outfiles):
             logging.info('CONVERTING: {}'.format(i))
-            convert(i,o,fs)
+            convert(i,o,fs, args.cores)
